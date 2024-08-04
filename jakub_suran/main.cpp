46fb1a315c4746e9e7f2d82dd95f8b9db242c7ef
jakubSuranUSM <123655266+jakubSuranUSM@users.noreply.github.com>
2024-08-04 14:51:41 +0000

    Final solution

--- jakub_suran/main.cpp
+++ jakub_suran/main.cpp
@@ -1,20 +1,35 @@
-class Solution {
-public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if (intervals.size() <= 0)
-            return {};
-        
-        sort(intervals.begin(), intervals.end());
+#include <vector>
+#include <algorithm>
+#include <cstdio>
 
-        vector<vector<int>> result;
-        result.push_back(intervals[0]);
+using namespace std;
 
-        for (int i = 1; i < intervals.size(); i++) {
-            if (result.back()[1] < intervals[i][0])
-                result.push_back(intervals[i]);
-            else
-                result.back()[1] = result.back()[1] > intervals[i][1] ? result.back()[1] : intervals[i][1];
-        }
-        return result;
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    if (intervals.size() <= 0)
+        return {};
+    
+    sort(intervals.begin(), intervals.end());
+
+    vector<vector<int>> result;
+    result.push_back(intervals[0]);
+
+    for (int i = 1; i < intervals.size(); i++) {
+        if (result.back()[1] < intervals[i][0])
+            result.push_back(intervals[i]);
+        else
+            result.back()[1] = result.back()[1] > intervals[i][1] ? result.back()[1] : intervals[i][1];
     }
-};
+    return result;
+}
+
+int main() {
+    vector<vector<int>> intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+    
+    vector<vector<int>> mergedIntervals = merge(intervals);
+    
+    for (const auto& interval : mergedIntervals) {
+        printf("[%d, %d] ", interval[0], interval[1]);
+    }
+    
+    return 0;
+}
